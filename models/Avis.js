b614b1ecc0dfb79db4933bcfb733729ede64dff8
SafiMoustapha <157801425+SafiMoustapha@users.noreply.github.com>
2025-03-27 20:01:03 +0100

    modif

--- models/Avis.js
+++ models/Avis.js
@@ -1,13 +1,28 @@
 const mongoose = require("mongoose");
 
 const avisSchema = new mongoose.Schema({
-    nom: { type: String, required: true },
-    email: { type: String, required: true },
-    type_avis: { type: String, required: true },
-    avis: { type: String, required: true },
-    note: { type: Number, required: true, min: 1, max: 5 },
-    document: { type: String }, // document est facultatif
+    nom: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    avis: {
+        type: String,
+        required: true
+    },
+    type_avis: {
+        type: String,
+        required: true
+    },
+    hopital: {
+        type: String,
+        required: true
+    }
 });
+
 
 const Avis = mongoose.model("Avis", avisSchema);
 module.exports = Avis;
